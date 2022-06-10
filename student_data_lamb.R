d23e91a300f785ae8d200002b171bfc8122d1b9e
LGhilotti <48059099+LGhilotti@users.noreply.github.com>
2022-06-10 10:13:45 +0200

    Update student_data_lamb.R

--- student_data_lamb.R
+++ student_data_lamb.R
@@ -20,9 +20,12 @@ thin= 5
 ## RUN ON DIFFERENT DATASETS AND SETTINGS ##
 ############################################
 
-p_s = c(100, 200, 400)
-d_s = c(2, 5, 10)
-M_s = c(4, 8, 12)
+#p_s = c(100, 200, 400)
+p_s = 400
+#d_s = c(2, 5, 10)
+d_s = 10
+#M_s = c(4, 8, 12)
+M_s = 12
 n_percluster_s = c(50)
 
 # Outer cycle for reading the different datasets and perform the estimation
@@ -66,7 +69,8 @@ for (p in p_s){
         diag_psi_iw=20
         niw_kap=1e-3
         nu=d+50
-        conc_dir_s = c(0.25, 0.5, 1)
+        #conc_dir_s = c(0.25, 0.5, 1)
+        conc_dir_s = c(0.5,1)
 
         for (conc_dir in conc_dir_s){
           #### Fit the `Lamb` Model
@@ -81,7 +85,8 @@ for (p in p_s){
           
           # Save results in folder
           base_outpath_conc = outpath_d + "/conc_{conc_dir}_out"
-          i = 0
+          #i = 0
+          i=1
           while (dir.exists(base_outpath_conc + "{i}")){
             i = i+1
           }
