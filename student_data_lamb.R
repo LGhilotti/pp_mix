6f8bef2435973fe0d7dc843798916cc995255c06
LGhilotti <48059099+LGhilotti@users.noreply.github.com>
2022-06-10 07:41:23 +0200

    Update student_data_lamb.R

--- student_data_lamb.R
+++ student_data_lamb.R
@@ -90,7 +90,7 @@ for (p in p_s){
           
           disc=nburn/thin
           post.samples=result.lamb[-(1:disc),]+1
-          write.table(post.samples, file = outpath + "alloc_matrix.csv" , 
+          write.table(post.samples, file = outpath + "/alloc_matrix.csv" , 
                       quote=FALSE, eol="\n", row.names=FALSE, col.names=FALSE,  sep=",")
           
           write(x=conc_dir, file = outpath + "/conc_param.txt")
