265816a6b853eca222e1e998ee82c89fb7a96516
mberaha <berahamario@gmail.com>
2022-08-31 21:11:58 +0000

    lamb longer almost completed

--- student_data_lamb.R
+++ student_data_lamb.R
@@ -11,9 +11,9 @@ sourceCpp("lamb_mod/DL_linear_split_merge_package.cpp") ##This is the souce C++ 
 #################
 set.seed(1234)
 
-nburn=1e3
-niter =25e3
-thin= 5
+nburn=5e4
+niter =20e3
+thin= 10
 
 
 ############################################
