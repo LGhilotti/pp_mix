c4ffb9f4cc2a5e66b76fc8c71dc4bb882c51fafe
mberaha <berahamario@gmail.com>
2022-12-08 16:04:04 +0000

    another try fixed

--- bauges_data_pp_mix.py
+++ bauges_data_pp_mix.py
@@ -81,7 +81,7 @@ if __name__ == "__main__" :
         ####################################
 
         # Set the expected number of centers a priori
-        rho_s = [20,30,40]
+        rho_s = [30,40,50]
 
         for rho in rho_s:
 
@@ -110,7 +110,7 @@ if __name__ == "__main__" :
             #hyperpar.wishart.nu = hyperpar.wishart.nu + d
 
             # different hyperquares side length
-            sidelength_s = [20,30,40]
+            sidelength_s = [30,40,50]
 
             for sidelength in sidelength_s:
 
